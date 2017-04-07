d1db121e4132bf6d0cf58fa287fda0d94f980b59
Srinivas Narayana <ngsrinivas@gmail.com>
2017-04-07 14:34:48 -0400

    Compile bug fixes

--- net/ipv4/tcp_testcubic.c
+++ net/ipv4/tcp_testcubic.c
@@ -2,9 +2,6 @@
 #include <net/tcp.h>
 #include <linux/math64.h>
 
-/* TODO */
-/* define parameters bictcp_hz and cube_rtt_scale and cube_factor and
- * beta and bictcp_beta_scale. */
 #define BICTCP_BETA_SCALE    1024	/* Scale factor beta calculation
 					 * max_cwnd = snd_cwnd * beta
 					 */
@@ -29,178 +26,8 @@ struct testcubic {
   u32 last_max_cwnd; /* the max window from the last cubic 'turnaround'.
                         Useful to compute the ssthresh and origin_cwnd next
                         time. */
+  u32 cnt;           /* cwnd/(additive increase to the congestion window) */
 };
-
-static void tcp_testcubic_cong_avoid(struct sock *sk, u32 ack, u32 acked)
-{
-  struct tcp_sock *tp = tcp_sk(sk);
-  struct testcubic *ca = inet_csk_ca(sk);
-
-  /* Standard boilerplate for cong_avoid functions */
-  if (!tcp_is_cwnd_limited(sk))
-    return;
-
-  if (tcp_in_slow_start(tp)) {
-    acked = tcp_slow_start(tp, acked);
-    if (! acked)
-      return;
-  }
-
-  /* Begin actual cubic update logic. */
-  u32 t, target, bic_target, delta, cwnd;
-  s32 offs;
-
-  /* Only update windows if something changed in the last jiffy. */
-  cwnd = tp->snd_cwnd;
-  if (ca->last_cwnd == cwnd &&
-	    (s32)(tcp_time_stamp - ca->last_time) <= HZ / 32)
-		return;
-
-  /* Save a few things before starting cwnd updates. */
-  ca->last_cwnd = cwnd;
-  ca->last_time = tcp_time_stamp;
-
-  /* Shamelessly copied cubic function arithmetic from cubic tcp
-     implementation. */
-	if (ca->epoch_start == 0) {
-		ca->epoch_start = tcp_time_stamp;	/* record beginning */
-
-		if (ca->last_max_cwnd <= cwnd) {
-			ca->bic_K = 0;
-			ca->origin_cwnd = cwnd;
-		} else {
-			/* Compute new K based on
-			 * (wmax-cwnd) * (srtt>>3 / HZ) / c * 2^(3*bictcp_HZ)
-			 */
-			ca->bic_K = cubic_root(cube_factor
-					       * (ca->last_max_cwnd - cwnd));
-			ca->origin_cwnd = ca->last_max_cwnd;
-		}
-	}
-
-  /* Here come the actual cubic updates with gory operations to avoid
-   * overflows. */
-  t = (s32)(tcp_time_stamp - ca->epoch_start);
-	t += msecs_to_jiffies(ca->delay_min >> 3);
-	/* change the unit from HZ to bictcp_HZ */
-	t <<= BICTCP_HZ;
-	do_div(t, HZ);
-
-	if (t < ca->bic_K)		/* t - K */
-		offs = ca->bic_K - t;
-	else
-		offs = t - ca->bic_K;
-
-	/* c/rtt * (t-K)^3 */
-	delta = (cube_rtt_scale * offs * offs * offs) >> (10+3*BICTCP_HZ);
-	if (t < ca->bic_K)                            /* below origin*/
-		bic_target = ca->origin_cwnd - delta;
-	else                                          /* above origin*/
-		bic_target = ca->origin_cwnd + delta;
-
-	/* cubic function - calc bictcp_cnt*/
-	if (bic_target > cwnd) {
-		ca->cnt = cwnd / (bic_target - cwnd);
-	} else {
-		ca->cnt = 100 * cwnd;              /* very small increment*/
-	}
-}
-
-static u32 tcp_testcubic_ssthresh(struct sock *sk)
-{
-  /* I wonder if there's an implicit assumption that this function is called
-   * whenever there is a loss... */
-  const struct tcp_sock *tp = tcp_sk(sk);
-  struct testcubic *ca = inet_csk_ca(sk);
-
-  /* Set the last max cwnd to current cwnd. */
-  ca->last_max_cwnd = tp->snd_cwnd;
-  return max((tp->snd_cwnd * beta) / BICTCP_BETA_SCALE, 2U);
-}
-
-static u32 tcp_testcubic_undo_cwnd(struct sock *sk)
-{
-  /* Still have to understand crystal-clearly where and how undo_cwnd is
-   * used. */
-  struct tcp_sock *tp = tcp_sk(sk);
-  struct testcubic *ca = inet_csk_ca(sk);
-
-  return max(tp->snd_cwnd, ca->last_max_cwnd);
-}
-
-static void tcp_testcubic_set_state(struct sock *sk, u8 new_state)
-{
-  if (new_state == TCP_CA_LOSS) {
-    tcp_testcubic_init();
-  }
-}
-
-/* This function may not even be needed for the preliminary implementation... */
-/* static void tcp_testcubic_cwnd_event(struct sock *sk, enum tcp_ca_event event) */
-/* { */
-/*   struct tcp_sock *tp = tcp_sk(sk); */
-/*   struct testcubic *ca = inet_csk_ca(sk); */
-
-/*   if (event == CA_EVENT_TX_START) { */
-/*     /\* Restarting transmission after an idle period... *\/ */
-/*     u32 now = tcp_time_stamp; */
-/*     s32 delta; */
-
-/*     delta = now - tp->lsndtime; */
-/*     if (ca->epoch_start && delta >= 0) { */
-/*       ca->epoch_start += delta; /\* adjust the cubic curve as though it's beginning */
-/*                                    from some time in the recent past, instead of a */
-/*                                    long time ago...*\/ */
-/*       if (after(ca->epoch_start, now)) */
-/*         ca->epoch_start = now; */
-/*     } */
-/*     return; */
-/*   } */
-/* } */
-
-static void tcp_testcubic_init(struct sock *sk)
-{
-  struct testcubic *testcubic = inet_csk_ca(sk);
-  testcubic->bic_K = 0;
-  testcubic->origin_cwnd = 0;
-  testcubic->epoch_start = 0;
-  testcubic->last_max_cwnd = 0;
-  testcubic->last_time = 0;
-  testcubic->last_cwnd = 0;
-}
-EXPORT_SYMBOL_GPL(tcp_testcubic_init);
-
-static struct tcp_congestion_ops tcp_testcubic = {
-  .cong_avoid = tcp_testcubic_cong_avoid,
-  .ssthresh = tcp_testcubic_ssthresh,
-  .undo_cwnd = tcp_testcubic_undo_cwnd,
-  .set_state = tcp_testcubic_set_state,
-  .init = tcp_testcubic_init,
-  /*  .cwnd_event = tcp_testcubic_cwnd_event, */
-
-  .owner = THIS_MODULE,
-  .name = "testcubic",
-};
-
-static int __init tcp_testcubic_register(void)
-{
-  printk(KERN_INFO "Initializing testcubic\n");
-  BUILD_BUG_ON(sizeof(struct testcubic) > ICSK_CA_PRIV_SIZE);
-
-  /* Boilerplate initializations from TCP's cubic implementations. */
-	cube_rtt_scale = (bic_scale * 10);	/* 1024*c/rtt */
-  cube_factor = 1ull << (10+3*BICTCP_HZ); /* 2^40 */
-  do_div(cube_factor, bic_scale * 10);
-  
-  tcp_register_congestion_control(&tcp_testcubic);
-  return 0;
-}
-
-static void __exit tcp_testcubic_unregister(void)
-{
-  printk(KERN_INFO "Exiting testcubic\n");
-  tcp_unregister_congestion_control(&tcp_testcubic);
-}
 
 /* calculate the cubic root of x using a table lookup followed by one
  * Newton-Raphson iteration.
@@ -248,6 +75,181 @@ static u32 cubic_root(u64 a)
 	x = (2 * x + (u32)div64_u64(a, (u64)x * (u64)(x - 1)));
 	x = ((x * 341) >> 10);
 	return x;
+}
+
+static void tcp_testcubic_cong_avoid(struct sock *sk, u32 ack, u32 acked)
+{
+  u32 bic_target, delta, cwnd;
+  u64 offs, t;
+
+  struct tcp_sock *tp = tcp_sk(sk);
+  struct testcubic *ca = inet_csk_ca(sk);
+
+  /* Standard boilerplate for cong_avoid functions */
+  if (!tcp_is_cwnd_limited(sk))
+    return;
+
+  if (tcp_in_slow_start(tp)) {
+    acked = tcp_slow_start(tp, acked);
+    if (! acked)
+      return;
+  }
+
+  /* Begin actual cubic update logic. */
+  /* Only update windows if something changed in the last jiffy. */
+  cwnd = tp->snd_cwnd;
+  if (ca->last_cwnd == cwnd &&
+	    (s32)(tcp_time_stamp - ca->last_time) <= HZ / 32)
+		return;
+
+  /* Save a few things before starting cwnd updates. */
+  ca->last_cwnd = cwnd;
+  ca->last_time = tcp_time_stamp;
+
+  /* Shamelessly copied cubic function arithmetic from cubic tcp
+     implementation. */
+	if (ca->epoch_start == 0) {
+		ca->epoch_start = tcp_time_stamp;	/* record beginning */
+
+		if (ca->last_max_cwnd <= cwnd) {
+			ca->bic_K = 0;
+			ca->origin_cwnd = cwnd;
+		} else {
+			/* Compute new K based on
+			 * (wmax-cwnd) * (srtt>>3 / HZ) / c * 2^(3*bictcp_HZ)
+			 */
+			ca->bic_K = cubic_root(cube_factor
+					       * (ca->last_max_cwnd - cwnd));
+			ca->origin_cwnd = ca->last_max_cwnd;
+		}
+	}
+
+  /* Here come the actual cubic updates with gory operations to avoid
+   * overflows. */
+  t = (s32)(tcp_time_stamp - ca->epoch_start);
+  /* ignoring adding the min_delay to t */
+	/* change the unit from HZ to bictcp_HZ */
+	t <<= BICTCP_HZ;
+	do_div(t, HZ);
+
+	if (t < ca->bic_K)		/* t - K */
+		offs = ca->bic_K - t;
+	else
+		offs = t - ca->bic_K;
+
+	/* c/rtt * (t-K)^3 */
+	delta = (cube_rtt_scale * offs * offs * offs) >> (10+3*BICTCP_HZ);
+	if (t < ca->bic_K)                            /* below origin*/
+		bic_target = ca->origin_cwnd - delta;
+	else                                          /* above origin*/
+		bic_target = ca->origin_cwnd + delta;
+
+	/* cubic function - calc bictcp_cnt*/
+	if (bic_target > cwnd) {
+		ca->cnt = cwnd / (bic_target - cwnd);
+	} else {
+		ca->cnt = 100 * cwnd;              /* very small increment*/
+	}
+}
+EXPORT_SYMBOL_GPL(tcp_testcubic_cong_avoid);
+
+static u32 tcp_testcubic_ssthresh(struct sock *sk)
+{
+  /* I wonder if there's an implicit assumption that this function is called
+   * whenever there is a loss... */
+  const struct tcp_sock *tp = tcp_sk(sk);
+  struct testcubic *ca = inet_csk_ca(sk);
+
+  /* Set the last max cwnd to current cwnd. */
+  ca->last_max_cwnd = tp->snd_cwnd;
+  return max((tp->snd_cwnd * beta) / BICTCP_BETA_SCALE, 2U);
+}
+EXPORT_SYMBOL_GPL(tcp_testcubic_ssthresh);
+
+static u32 tcp_testcubic_undo_cwnd(struct sock *sk)
+{
+  /* Still have to understand crystal-clearly where and how undo_cwnd is
+   * used. */
+  struct tcp_sock *tp = tcp_sk(sk);
+  struct testcubic *ca = inet_csk_ca(sk);
+
+  return max(tp->snd_cwnd, ca->last_max_cwnd);
+}
+EXPORT_SYMBOL_GPL(tcp_testcubic_undo_cwnd);
+
+/* This function may not even be needed for the preliminary implementation... */
+/* static void tcp_testcubic_cwnd_event(struct sock *sk, enum tcp_ca_event event) */
+/* { */
+/*   struct tcp_sock *tp = tcp_sk(sk); */
+/*   struct testcubic *ca = inet_csk_ca(sk); */
+
+/*   if (event == CA_EVENT_TX_START) { */
+/*     /\* Restarting transmission after an idle period... *\/ */
+/*     u32 now = tcp_time_stamp; */
+/*     s32 delta; */
+
+/*     delta = now - tp->lsndtime; */
+/*     if (ca->epoch_start && delta >= 0) { */
+/*       ca->epoch_start += delta; /\* adjust the cubic curve as though it's beginning */
+/*                                    from some time in the recent past, instead of a */
+/*                                    long time ago...*\/ */
+/*       if (after(ca->epoch_start, now)) */
+/*         ca->epoch_start = now; */
+/*     } */
+/*     return; */
+/*   } */
+/* } */
+
+static void tcp_testcubic_init(struct sock *sk)
+{
+  struct testcubic *testcubic = inet_csk_ca(sk);
+  testcubic->bic_K = 0;
+  testcubic->origin_cwnd = 0;
+  testcubic->epoch_start = 0;
+  testcubic->last_max_cwnd = 0;
+  testcubic->last_time = 0;
+  testcubic->last_cwnd = 0;
+}
+EXPORT_SYMBOL_GPL(tcp_testcubic_init);
+
+static void tcp_testcubic_set_state(struct sock *sk, u8 new_state)
+{
+  if (new_state == TCP_CA_Loss) {
+    tcp_testcubic_init(sk);
+  }
+}
+EXPORT_SYMBOL_GPL(tcp_testcubic_set_state);
+
+static struct tcp_congestion_ops tcp_testcubic = {
+  .cong_avoid = tcp_testcubic_cong_avoid,
+  .ssthresh = tcp_testcubic_ssthresh,
+  .undo_cwnd = tcp_testcubic_undo_cwnd,
+  .set_state = tcp_testcubic_set_state,
+  .init = tcp_testcubic_init,
+  /*  .cwnd_event = tcp_testcubic_cwnd_event, */
+
+  .owner = THIS_MODULE,
+  .name = "testcubic",
+};
+
+static int __init tcp_testcubic_register(void)
+{
+  printk(KERN_INFO "Initializing testcubic\n");
+  BUILD_BUG_ON(sizeof(struct testcubic) > ICSK_CA_PRIV_SIZE);
+
+  /* Boilerplate initializations from TCP's cubic implementations. */
+	cube_rtt_scale = (bic_scale * 10);	/* 1024*c/rtt */
+  cube_factor = 1ull << (10+3*BICTCP_HZ); /* 2^40 */
+  do_div(cube_factor, bic_scale * 10);
+  
+  tcp_register_congestion_control(&tcp_testcubic);
+  return 0;
+}
+
+static void __exit tcp_testcubic_unregister(void)
+{
+  printk(KERN_INFO "Exiting testcubic\n");
+  tcp_unregister_congestion_control(&tcp_testcubic);
 }
 
 module_init(tcp_testcubic_register);
